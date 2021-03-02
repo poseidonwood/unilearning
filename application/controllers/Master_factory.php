cf50810a09e315f2a22648b86b8fdf4f22f8705d
Poseidonwood <santosofebrikukuh@gmail.com>
2021-03-02 09:28:41 +0700

    adding master factory

--- application/controllers/Master_factory.php
+++ application/controllers/Master_factory.php
@@ -9,8 +9,27 @@ class Master_factory extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        //Load Dependencies
-
+        $this->load->model('MNotif');
+        $this->load->model('MLogin');
+        $this->load->model('MData');
+        $maintenance = $this->MData->selectmaintenance('maintenance', array('status' => 'T'));
+        if ($maintenance !== FALSE) {
+            redirect("maintenance", 'refresh');
+        }
+        $this->load->library('Excel');
+        $this->load->model('ModThirdapp');
+        if ($this->session->userdata('logged_in') !== TRUE) {
+            $this->session->set_flashdata('error', 'HARAP LOGIN DAHULU');
+            redirect("login?continue=" . current_url(), 'refresh');
+        } else {
+            $role = $this->session->userdata('role');
+            // if ($role !== 'ADMIN' || $role !== 'SUPERADMIN') {
+            //     $rolenya = $this->session->userdata('role');
+            //     $this->session->sess_destroy();
+            //     $this->session->set_flashdata('error', 'AKSES DI TOLAK SILAHKAN LOGIN LAGI ' . $rolenya);
+            //     redirect('login');
+            // }
+        }
     }
 
     // List all your items
